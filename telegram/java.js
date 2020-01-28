d2a9045f47e48b93914969822a595c62e494162f
lsilva2019crn <38224089+lsilva2019crn@users.noreply.github.com>
2020-01-28 18:50:13 +0100

    Add files via upload

---
+++ telegram/java.js
@@ -0,0 +1,12 @@
+
+
+var color=["turquoise","aquamarine","pink","orange","yellow","green"];
+
+var bola=document.getElementsByClassName('bola');
+
+bola[0].style.backgroundColor=color[0];
+bola[1].style.backgroundColor=color[1];
+bola[2].style.backgroundColor=color[2];
+bola[3].style.backgroundColor=color[3];
+bola[4].style.backgroundColor=color[4];
+bola[5].style.backgroundColor=color[5];
